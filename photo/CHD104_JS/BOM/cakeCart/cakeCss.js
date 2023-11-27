55c98c2c6af9b4012e95b57188cce712eb67f5fc
Min-Syuan <annie0910610569gmail.com>
2023-11-27 15:27:23 +0800

    註解

--- photo/CHD104_JS/BOM/cakeCart/cakeCss.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load", function(){
-	//-------------------
-	document.getElementById("btnMinus0").onclick = function(){
-		
-	}
-})
--- photo/CHD104_JS/func_global_local.js
+++
@@ -1,8 +0,0 @@
-//危險的範例
-function sum(){
-    total = 0;
-    for(i = 0; i<arguments.length; i++){
-        total += arguments[i];
-    }
-    return total;
-}
